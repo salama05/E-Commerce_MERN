e53611fca7c2f5150800e10a1556e7cd0244f6ab
salama <salamaess@hotmail.fr>
2025-07-03 22:56:58 +0100

    All Product in Home page + integration with API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,12 +5,14 @@ import userRoute from "./routes/userRoute";
 import { seedInitialProducts } from "./services/productService";
 import productRoute from "./routes/productRoute";
 import cartRoute from "./routes/cartRoute";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const port = 3001;
 
 app.use(express.json());
+app.use(cors());
 
 mongoose
   .connect(process.env.DATABASE_URL || "")
--- backend/src/services/productService.ts
+++ backend/src/services/productService.ts
@@ -14,6 +14,20 @@ export const seedInitialProducts = async () => {
         price: 14000,
         stock: 90,
       },
+      {
+        title: "Asus Laptop",
+        image:
+          "https://hardsoft.dz/images/articles/1453/1698743473-ASUS-X515EP-1.png",
+        price: 13500,
+        stock: 70,
+      },
+      {
+        title: "HP Laptop",
+        image:
+          "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c08428581.png",
+        price: 14500,
+        stock: 96,
+      },
     ];
 
     const existingProducts = await getAllProducts();
